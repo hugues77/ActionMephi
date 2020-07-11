443f2880416fb8dca05e1244db13e9ecf02a25e7
Hugues eyong <hugues777@outlook.com>
2020-07-11 05:17:41 +0200

    Notre header et footer pour le projet de action mephi

---
+++ includes/footer.php
@@ -0,0 +1,130 @@
+</body>
+<!-- Footer -->
+<footer class="page-footer font-small  lighten-3 pt-4">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-4 mr-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Content -->
+        <h5 class="font-weight-bold text-uppercase mb-4 text-justify">Action Mephibochet</h5>
+        <p class="text-justify">Here you can use rows and columns to organize your footer content.<br>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate
+          esse
+          quasi, veritatis totam voluptas nostrum.</p>
+
+      </div>
+
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Contact details -->
+        <h5 class="font-weight-bold text-uppercase mb-4">Siège sociale</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+            <i class="fas fa-home"></i> Cologne, Köln 50667, DE</p>
+          </li>
+          <li>
+            <p>
+            <i class="fas fa-envelope-open-text"></i> info@actionmephi.com</p>
+          </li>
+          <li>
+            <p>
+            <i class="fas fa-phone-alt"></i> +49 221 98 62 50 80</p>
+          </li>
+          <li>
+            <p>
+            <i class="fab fa-whatsapp"></i>+ 49 1 60 96 32 97 04</p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+      <!-- Grid column -->    
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mb-4">Liens Utiles</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <a href="#!">PROJETS</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#!">A PROPOS</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#!">FAIRE UN DON</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#!">CONTACT</a>
+            </p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+      <hr class="clearfix w-100 d-md-none">
+      <div class="col-md-3 col-lg-3 text-center mx-auto my-4">
+
+        <!-- Social buttons -->
+        <h5 class="font-weight-bold text-uppercase mb-4">Facebook Page</h5>
+
+        <!-- Facebook -->
+          <div class="">
+          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAktion-Mephibochet-eV-272890542808364%2F%3F__tn__%3D%252Cd%252CP-R%26eid%3DARCm53sdO177uonQMCTwapcMWRIGRBAfmQndRDORObFd1x_-7-g3wGk2nGPmmivKBFGddoUnnXKs5uH7&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
+            width="300" height="200" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+          </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center bg-footer py-3">© 2020 Copyright:
+    <a href="https://api.whatsapp.com/send?phone=33651294692&text=&source=&data=">
+        <strong class="text-light"> Softhandy.fr</strong>
+    </a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    <script src="js/index.js"></script>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,78 @@
+<?php
+$title = "Bienvenue ACtion Mephi";
+$description = "Bienvenue dans le site de Action Mephibochet";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title><?=$title?></title>
+  <meta name="description" content="<?=$description ?>">
+  <meta name="Keywords" content="handicap">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="shortcut icon" href="favicon.ico">
+  <link href="https://fonts.googleapis.com/css2?family=Francois+One&family=Montserrat+Subrayada:wght@700&family=Roboto:ital@1&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/bdf8dcd65d.js"></script>
+  
+</head>
+<body>
+        
+    <div class="fixed-top">    
+        <div class="container-fluid ">
+            <div class="row bg-footer p-1">
+                <div class="col-xs-5 col-md-5 col-lg-5">
+                    <a href=""><img src="img/facebook.png" width="35px" height="" alt=""></a>
+                    <a href=""><img src="img/instagram.png" width="35px" height="" alt=""></a>
+                    <a href=""><img src="img/twitter.png" width="35px" height="" alt=""></a>
+                    <a href=""><img src="img/google.png" width="35px" height="" alt=""></a>
+                    <a href=""><img src="img/youtube.png" width="35px" height="" alt=""></a>
+                </div>
+                <div class="col-xs-7 col-md-7 col-lg-7 text-light float-right ">
+                    <div class="row ">
+                        <div class="col-xs-4 col-md-4 col-lg-4  mt-2 heure text-right">
+                            <a class="mel" href="tel:+4922198625080"><i class="fas fa-headset mr-2"></i>+49 221 98 62 50 80 </a>
+                        </div>
+                        <div class="col-xs-4 col-md-4 col-lg-4  mt-2 heure">
+                            <a class="mel" href="mailto:info@actionmephi.com"><i class="fas fa-envelope-open-text mr-2"></i>info@actionmephi.com</a>
+                        </div>
+                        <div class="col-xs-4 col-md-4 col-lg-4">
+                            <form class="form-inline my-2 my-lg-0 heure">
+                                <button class="horange text-light my-2 my-sm-0" type="submit"><i class="fas fa-hand-holding-heart mr-2"></i> JE FAIS UN DON</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <header>
+            <div class="main">
+                <div class="logo">
+                    <a href="#"><img class="pl-5" src="img/ActionMephi.png" width="250px" height="50px" alt=""></a>
+                </div>
+                <ul class="float-right mt-3"> 
+                    <li class="active"><a href="#" class="active">Home</a></li>
+                    <li class=""><a href="#">A propos</a></li>
+                    <li class=""><a href="#">Medias/Gallery</a></li>
+                    <li class=""><a href="#">Projet</a></li>
+                    <li class=""> <a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="titre">
+                <strong><h1 class="">ACTION MEPHIBOCHET</h1></strong>
+                <p class="text-light descript">Bienvenue dans le site Officiel de ACtion Mephi, retrouvez l'ensemble de nos informations au travers notre plateforme;
+                    Vous pouvez aussi nous écrire et/ou nous venir en aide via nos differents liens respectifs. Bonne navigation !
+                </p>
+                <div class="row">
+                    <div class="col-md-6">
+                        <button class="btn btn-danger btn-block">Nous Sommes</button>
+                    </div>
+                    <div class="col-md-6">
+                        <button class="btn btn-outline-info btn-block">Nous Contactez</button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+
