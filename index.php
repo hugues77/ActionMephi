00c1c08ad46e890c9c9ded3e63e7df2fbb73dc53
hugues77 <hugues777@outlook.com>
2021-03-09 18:45:22 +0100

    création des routes et des autres pages

--- index.php
+++ index.php
@@ -1,92 +1,57 @@
 <?php
-require_once 'includes/header.php'; ?>
-<section class="horange"  id="propos">
-    <div class="container">
-        <h1>A propos</h1>   
-            <p class="text-justify text-light descript">
-                L'association est active de manière désintéressée, elle ne poursuit pas principalement les activités économiques,
-                Cependant, Il devrait être ouvert à l'intégration et à la coopération d'autres organisations qui s'occupent des personnes handicapées; <br>
-                Il s'agit aussi d'échange d'informations, d'opinions et d'expériences entre différentes cultures. Orientation et instruction des jeunes handicapés et non handicapés d'origine africaine à Cologne dans divers instruments de musique africains. Cours d'Évangile pour jeunes et adultes 
-                Elle contient plusieurs objectifs et s'oriente aussi pour plusieures promotions.<small class="btn btn-outline-light ml-2 mt-1"> <i class="fas fa-arrow-right mr-1"></i>  Lire la suite</small>
-            </p>  
-            <div class="row">
-                <div class="col-md-4 ">
-                    <div class="text-center mt-4 mb-2">
-                        <i class="fas fa-wheelchair mb-2 fa-4x text-dark"></i>
-                    </div>
-                    <h5 class="text-light">Promotion de l'aide aux personnes handicapées </h5>
-                </div>
-                <div class="col-md-4">
-                    <div class="text-center mt-4 ">
-                        <i class="fas fa-globe-americas mb-2  fa-4x text-dark"></i>
-                    </div>
-                    <h5 class="text-light text-left">Promotion des sentiments internationaux, de la tolérance dans tous les domaines de la culture et de la compréhension entre les peuples</h5>
-                </div>
-                <div class="col-md-4">
-                    <div class="text-center mt-4 mb-2">
-                        <i class="fas fa-child  mb-2 fa-4x text-dark"></i>
-                    </div>
-                    <h5 class="text-light">Promotion de l'aide à la jeunesse.</h5>
-                </div>
-            </div>       
-    </div>   
-</section>
-<section class="bg-info">
-    <div class="container">
-        <h1 class="">Actualités au sein d'Action Mephi</h1>
-        <div class="row">
-            <div class="col-md-6">
-                <img class="filtre mt-4" src="img/mephi_actualite.jpg" class="img-thumbnail mb-2" width="100%" height="110%" alt="">
-            </div>
-            <div class="col-md-6 actua">
-                <p class="text-justify ">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex porro voluptas nobis veniam ipsam earum ut amet iste nam, optio, velit assumenda accusamus officiis. Voluptates in perspiciatis modi aspernatur excepturi.
-                </p>
-                <div class="row mt-4 mb-3">
-                    <div class="col-md-8 offset-2">
-                        <a href="#" class="btn btn-outline-light btn-lg btn-block"><i class="fas fa-plus mr-2"></i>Plus d-infos</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<section class="horange">
-    <div class="container">
-        <h1 class="mt-4">Action Mephi en Images</h1>
-        <div class="row">
-            <div class="col-md-3">
-                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
-            </div>
-            <div class="col-md-3">
-                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
-            </div>
-            <div class="col-md-3">
-                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
-            </div>
-            <div class="col-md-3">
-                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
-            </div>
-        </div>
-        <div class="row mt-4">
-            <div class="col-md-3">
-                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
-            </div>
-            <div class="col-md-3">
-                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
-            </div>
-            <div class="col-md-3">
-                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
-            </div>
-            <div class="col-md-3">
-                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
-            </div>
-        </div>
-        <div class="row mt-4 mb-3">
-            <div class="col-md-4 offset-4">
-                <a href="#" class="btn btn-outline-info btn-lg btn-block"><i class="fas fa-plus mr-2"></i>Voir plus</a>
-            </div>
-        </div>
-    </div>
-</section>
-<?php require_once 'includes/footer.php';
+
+session_start();
+require_once 'functions/functions.php';
+require_once 'admin/function/login.func.php';
+require_once 'functions/main.php';
+require_once 'includes/header.php';
+
+
+//var_dump($_GET);
+$url = '';
+if(isset($_GET['url'])){
+    $url = explode('/', $_GET['url']);
+}
+//var_dump($url);
+
+if($url ===''){
+    require 'pages/home.php';
+}elseif($url[0] =='media'){
+    require 'pages/gallery.php';
+} elseif($url[0] ==='nous-contacter'){
+    require 'pages/contact.php';
+} elseif($url[0] ==='nos-projets'){
+    require 'pages/projet.php';
+} elseif($url[0] ==='a-propos'){
+    require 'pages/propos.php';
+}
+elseif($url[0] ==='login'){
+    require 'pages/login.php';
+}
+elseif($url[0] ==='inscription'){
+    require 'pages/register.php';
+}
+elseif($url[0] ==='admin' && $url[1] =='login'){
+    require 'admin/pages/login.php';
+}
+elseif($url[0] ==='admin' && $url[1] =='dashboard'){
+    require 'admin/pages/dashboard.php';
+}
+elseif($url[0] ==='admin' && $url[1] =='logout'){
+    require 'admin/pages/logout.php';
+}
+elseif($url[0] ==='admin' && $url[1] =='mon-profil'){
+    require 'admin/pages/profil.php';
+}
+elseif($url[0] ==='admin' && $url[1] =='reglages'){
+    require 'admin/pages/reglages.php';
+}
+elseif($url[0] ==='admin' && $url[1] =='modification'){
+    require 'admin/pages/modification.php';
+}
+else{
+    require 'pages/error.php';
+}
+
+
+require_once 'includes/footer.php';
---
+++ pages/home.php
@@ -0,0 +1,89 @@
+<section class="horange"  id="propos">
+    <div class="container">
+        <h1>A propos</h1>   
+            <p class="text-justify text-light descript">
+                L'association est active de manière désintéressée, elle ne poursuit pas principalement les activités économiques,
+                Cependant, Il devrait être ouvert à l'intégration et à la coopération d'autres organisations qui s'occupent des personnes handicapées; <br>
+                Il s'agit aussi d'échange d'informations, d'opinions et d'expériences entre différentes cultures. Orientation et instruction des jeunes handicapés et non handicapés d'origine africaine à Cologne dans divers instruments de musique africains. Cours d'Évangile pour jeunes et adultes 
+                Elle contient plusieurs objectifs et s'oriente aussi pour plusieures promotions.<small class="btn btn-outline-light ml-2 mt-1"> <i class="fas fa-arrow-right mr-1"></i>  Lire la suite</small>
+            </p>  
+            <div class="row">
+                <div class="col-md-4 ">
+                    <div class="text-center mt-4 mb-2">
+                        <i class="fas fa-wheelchair mb-2 fa-4x text-dark"></i>
+                    </div>
+                    <h5 class="text-light">Promotion de l'aide aux personnes handicapées </h5>
+                </div>
+                <div class="col-md-4">
+                    <div class="text-center mt-4 ">
+                        <i class="fas fa-globe-americas mb-2  fa-4x text-dark"></i>
+                    </div>
+                    <h5 class="text-light text-left">Promotion des sentiments internationaux, de la tolérance dans tous les domaines de la culture et de la compréhension entre les peuples</h5>
+                </div>
+                <div class="col-md-4">
+                    <div class="text-center mt-4 mb-2">
+                        <i class="fas fa-child  mb-2 fa-4x text-dark"></i>
+                    </div>
+                    <h5 class="text-light">Promotion de l'aide à la jeunesse.</h5>
+                </div>
+            </div>       
+    </div>   
+</section>
+<section class="bg-info">
+    <div class="container">
+        <h1 class="">Actualités au sein d'Action Mephi</h1>
+        <div class="row">
+            <div class="col-md-6">
+                <img class="filtre mt-4" src="img/mephi_actualite.jpg" class="img-thumbnail mb-2" width="100%" height="110%" alt="">
+            </div>
+            <div class="col-md-6 actua">
+                <p class="text-justify ">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex porro voluptas nobis veniam ipsam earum ut amet iste nam, optio, velit assumenda accusamus officiis. Voluptates in perspiciatis modi aspernatur excepturi.
+                </p>
+                <div class="row mt-4 mb-3">
+                    <div class="col-md-8 offset-2">
+                        <a href="#" class="btn btn-outline-light btn-lg btn-block"><i class="fas fa-plus mr-2"></i>Plus d-infos</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="horange">
+    <div class="container">
+        <h1 class="mt-4">Action Mephi en Images</h1>
+        <div class="row">
+            <div class="col-md-3">
+                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
+            </div>
+            <div class="col-md-3">
+                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
+            </div>
+            <div class="col-md-3">
+                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
+            </div>
+            <div class="col-md-3">
+                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-3">
+                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
+            </div>
+            <div class="col-md-3">
+                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
+            </div>
+            <div class="col-md-3">
+                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
+            </div>
+            <div class="col-md-3">
+                <img class="filtre mt-1" src="img/ikebaali.jpg" class="img-thumbnail mb-2" width="100%" alt="action mephi">
+            </div>
+        </div>
+        <div class="row mt-4 mb-3">
+            <div class="col-md-4 offset-4">
+                <a href="#" class="btn btn-outline-info btn-lg btn-block"><i class="fas fa-plus mr-2"></i>Voir plus</a>
+            </div>
+        </div>
+    </div>
+</section>
